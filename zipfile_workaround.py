38be6e5c7ce408ed103eaf2070dbf03a50df36a2
sanderjo <sander.jonkers+github@gmail.com>
2016-05-16 22:37:27 +0200

    error message in code

--- zipfile_workaround.py
+++ zipfile_workaround.py
@@ -16,6 +16,14 @@ for _file in files:
 	print "file is", _file
 	try:
 		print "join is", os.path.join(basedir, _file)
+		'''
+		The above can give this error with Windows zipped files with special characters in them:
+
+		    print "join is", os.path.join(basedir, _file)
+			  File "/usr/lib/python2.7/posixpath.py", line 80, in join
+		    path += '/' + b
+		'''
+
 	except:
 		# the encoding was a problem, so let's assume it was a zip with special CP437 characters in the filename
 		print "join is", os.path.join(basedir, _file.decode('cp437'))
